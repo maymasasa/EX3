03097c4e374e41f6bf764b2e7b077455cc006bd8
“maymasasa” <soozmas@gmail.com>
2022-06-23 13:27:26 +0300

    1

--- app/src/main/java/com/example/ex3/Chat.java
+++ app/src/main/java/com/example/ex3/Chat.java
@@ -6,16 +6,12 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class Chat extends AppCompatActivity {
-    private AppDB db;
-    private UserDao userDao;
-    private MessageDao messageDao;
     ListView lvUsers;
     private List<User> users;
     ArrayAdapter<User> adapter;
@@ -24,9 +20,6 @@ public class Chat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        db = Room.databaseBuilder(getApplicationContext(),AppDB.class, "UsersDB").allowMainThreadQueries().build();
-        messageDao = db.messageDao();
-        userDao = db.userDao();
         FloatingActionButton addBtn = findViewById(R.id.btnAdd);
         addBtn.setOnClickListener(view -> {
             Intent intent = new Intent(this, AddFriend.class);
@@ -37,24 +30,11 @@ public class Chat extends AppCompatActivity {
             Intent intent = new Intent(this, Settings.class);
             startActivity(intent);
         });
-        users = userDao.index();
         lvUsers = findViewById(R.id.lvUsers);
-        adapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_1,users);
-        lvUsers.setAdapter(adapter);
-        lvUsers.setOnItemLongClickListener((adapterView, view, i, l) -> {
-             User user = users.remove(i);
-             userDao.delete(user);
-             adapter.notifyDataSetChanged();
-             return true;
-             });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        users.clear();
-        users.addAll(userDao.index());
-        adapter.notifyDataSetChanged();
-
     }
 }
--- app/src/main/java/com/example/ex3/User.java
+++ app/src/main/java/com/example/ex3/User.java
@@ -8,6 +8,9 @@ public class User {
     @PrimaryKey(autoGenerate = true)
     int id;
     String name;
+    String lastMessage;
+    String time;
+    int pic;
 
     public int getId() {
         return id;
@@ -29,6 +32,8 @@ public class User {
     public User(int id, String name) {
         this.id = id;
         this.name = name;
+        this.lastMessage = "last Message";
+        this.time = "11:11";
     }
 
     public User() {
@@ -37,6 +42,6 @@ public class User {
 
     @Override
     public String toString() {
-        return name + "\n\n" + "11:11" + "          " + "Last Message";
+        return name + "\n\n" + time + "          " + lastMessage;
     }
 }
